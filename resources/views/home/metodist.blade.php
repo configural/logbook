e850eda58d7cda509cab3b28d427c6fdbd021fef
Artem Kashkanov <kashkanov_as@mail.ru>
2020-12-30 10:49:28 +0300

    2020-12-30

--- resources/views/home/metodist.blade.php
+++ resources/views/home/metodist.blade.php
@@ -4,7 +4,10 @@
                         <hr>
                         <div class="icon"><a href="{{route('users')}}"><i class="fa fa-user fa-3x red"></i><br/>Пользователи системы</a></div>
                         <div class="icon"><a href="{{ route('streams') }}"><i class="fa fa-users fa-3x blue"></i><br/>Потоки, группы, слушатели</a></div>                    
-                        <div class="icon"><a href="{{ route('workload') }}"><i class="fa fa-pie-chart fa-3x orange"></i><br/>Распределение нагрузки</a></div>
+                        <div class="icon"><a href="{{ route('workload') }}"><i class="fa fa-pie-chart fa-3x orange"></i><br/>Распределение аудиторной нагрузки</a></div>
+                        <div class="icon"><a href="{{route('vneaud')}}"><i class="fa fa-pie-chart fa-3x blue"></i><br/>Внеаудиторная нагрузка</a></div>
+
+                        
                         <div class="icon"><a href="{{ route('rasp') }}"><i class="fa fa-calendar fa-3x brown"></i><br/>Расписание</a></div>
                 
                         <h3>Отчеты и документы</h3>
