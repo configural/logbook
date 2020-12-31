222f35b4aa1ced6f655a1844a7563af5caf80fff
Artem Kashkanov <kashkanov_as@mail.ru>
2020-12-31 10:46:42 +0300

    2020-12-31

--- resources/views/useradd.blade.php
+++ resources/views/useradd.blade.php
@@ -8,7 +8,7 @@
             <div class="panel panel-primary">
                 <div class="panel-heading ">Создание пользователя</div>
                 <div class="panel-body">
-                @if(Auth::user()->role_id == 4)    
+                @if(in_array(Auth::user()->role_id, [3, 4]))    
                 <form action="add" method="post">
                           
                           <p><label>ФИО</label><input type="text" value="" class="form-control" name="name" required></p>
